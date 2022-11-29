dd5d567a919d4f8e93e4a3f46d96f0099408d84f
Divya Lakshmi <69458308+Divyatech-hub@users.noreply.github.com>
2022-11-29 01:25:49 -0500

    Add files via upload

---
+++ Lab9/application.py
@@ -0,0 +1,43 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configuring the application
+App = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+App.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+
+@App.route("/", methods=["GET", "POST"])
+def index():
+
+    if request.method == "POST":
+        message = ""
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        if not name:
+            message = "Missing name"
+        elif not month:
+            message = "Missing month"
+        elif not day:
+            message = "Missing day"
+        else:
+            db.execute(
+                "INSERT INTO birthdays (name, month, day) VALUES(?, ?,?)",
+                name,
+                month,
+                day,
+            )
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", message = message, birthdays = birthdays)
+    else:
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays = birthdays)
+
+
